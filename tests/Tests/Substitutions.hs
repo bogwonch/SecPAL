3e45853229915e374abcda06f88871fff9ac0d56
Joseph Hallett <josephhallett@gmail.com>
2014-02-11 19:27:11 +0000

    Added missing substitution case

---
+++ tests/Tests/Substitutions.hs
@@ -0,0 +1,12 @@
+module Tests.Substitutions where
+
+import Text.Parsec
+import Logic.SecPAL.Language
+import Logic.SecPAL.Parser
+import Logic.SecPAL.Substitutions
+import Data.Either
+import Tests.Evaluation (makeAssertion)
+
+  
+
+
