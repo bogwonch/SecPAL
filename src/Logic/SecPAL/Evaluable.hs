321d61a7fd91926c69b039eeaa16fd08264f5d73
Joseph Hallett <josephhallett@gmail.com>
2014-04-09 18:09:13 +0100

    Rewrote ||- to be in the IO Monad and use randomize solution search

--- src/Logic/SecPAL/Evaluable.hs
+++ src/Logic/SecPAL/Evaluable.hs
@@ -1,69 +1,115 @@
 {-# OPTIONS -Wall #-}
 module Logic.SecPAL.Evaluable where
 
-import Logic.SecPAL.Language
-import Logic.SecPAL.Context
+import Control.Applicative
+import Control.Monad (when, forM)
+import Data.Array.IO
+import Data.Maybe
 import Logic.SecPAL.AssertionSafety (flat)
+import Logic.SecPAL.Context
+import Logic.SecPAL.Language
+import Logic.SecPAL.Pretty
 import Logic.SecPAL.Proof hiding (constraint, delegation)
 import Logic.SecPAL.Substitutions
-import Data.Maybe
-
-import Debug.Trace
-import Logic.SecPAL.Pretty
 import System.Console.ANSI
+import System.Random
+
 
 class Evaluable x where 
-    (||-) :: Context -> x -> Maybe (Proof x)
+    (||-) :: Context -> x -> IO (Maybe (Proof x))
 
 instance Evaluable C where
-  ctx ||- c@(Boolean True) = Just $ PStated (ctx,c)
-  _ ||- (Boolean False) = Nothing
+  ctx ||- c@(Boolean True) = return . Just $ PStated (ctx,c)
+  _ ||- (Boolean False) = return Nothing
 
   ctx ||- c@(Equals a b)
-    | a == b = Just $ PStated (ctx,c)
-    | otherwise = Nothing
+    | a == b = return . Just $ PStated (ctx,c)
+    | otherwise = return Nothing
 
-  ctx ||- c@(Not c') = 
-    let p = isJust $ ctx ||- c'
-    in if p 
-         then Just $ PStated (ctx,c)
-         else Nothing
+  ctx ||- c@(Not c') = do
+    p <- isJust <$> ctx ||- c'
+    if p 
+      then return . Just $ PStated (ctx,c)
+      else return Nothing
 
-  ctx ||- c@(Conj x y) =
-    let pX = isJust $ ctx ||- x
-        pY = isJust $ ctx ||- y
-    in if pX && pY 
-         then Just $ PStated (ctx,c)
-         else Nothing
+  ctx ||- c@(Conj x y) = do
+    pX <- isJust <$> ctx ||- x
+    pY <- isJust <$> ctx ||- y
+    return $ if pX && pY 
+      then Just $ PStated (ctx,c)
+      else Nothing
 
 
+type Result = Maybe (Proof Assertion)
 instance Evaluable Assertion where
-    ctx ||- x =
-      if isJust tS 
-        then ifdebugT "is known" tS
-        else ifdebugF "not known fact." $
-          if isJust tC 
-            then ifdebugT "is conditionally known" tC
-            else ifdebugF "not conditionally known" $
-              if isJust tCS 
-                then ifdebugT "is delegatable" tCS 
-                else ifdebugF "not delegatable" Nothing
-      where  
-        tS = tryStated ctx x
-        tC = tryCond ctx x 
-        tCS = tryCanSay ctx x
+  ctx ||- x = do
+    {- We're shuffling them so that we try and avoid infinite loops where a
+     - cond statement will loop but a cansay will terminate. 
+     -
+     - Probably a neater way of doing this but randomization is cheap and
+     - usually good!
+     -}
+    strategies <- shuffle [statedStrategy, condStrategy, canSayStrategy]
+    tryStrategies ctx x strategies 
+    where
+      -- | Randomly shuffle a list
+      --   /O(N)/
+      shuffle :: [a] -> IO [a]
+      shuffle xs = do
+              ar <- makeArray len xs
+              forM [1..len] $ \i -> do
+                  j <- randomRIO (i,len)
+                  vi <- readArray ar i
+                  vj <- readArray ar j
+                  writeArray ar j vi
+                  return vj
+        where
+          len = length xs
+          makeArray :: Int -> [a] -> IO (IOArray Int a)
+          makeArray l = newListArray (1,l)
 
-        ifdebugT message = 
-          if debug ctx
-            then trace $ inColor Green ("@ '"++pShow x++"' "++message)
-            else id
 
-        ifdebugF message = 
-          if debug ctx
-            then trace $ inColor Red ("@ '"++pShow x++"' "++message)
-            else id
-        
-        inColor c str = setSGRCode [SetColor Foreground Dull c] ++ str ++ setSGRCode[Reset]
+type Strategy = Context -> Assertion -> IO Result
+
+statedStrategy :: Strategy
+statedStrategy = strategy tryStated "known fact"
+
+condStrategy :: Strategy
+condStrategy   = strategy tryCond   "derivable fact"
+
+canSayStrategy :: Strategy
+canSayStrategy = strategy tryCanSay "delegatable"
+
+strategy :: (Context -> Assertion -> IO Result)
+         -> String
+         -> Context
+         -> Assertion
+         -> IO Result
+strategy f msg ctx x = do
+  result <- f ctx x
+  let isSuccessful = isJust result
+
+  when (debug ctx) $
+    putStrLn . useColor isSuccessful $ 
+      "@ '"++pShow x++"' "++usePrefix isSuccessful++" "++msg
+
+  return $ if isSuccessful then result else Nothing
+  where 
+    inColor c str = setSGRCode [SetColor Foreground Dull c] ++ str ++ setSGRCode[Reset]
+    
+    useColor True   = inColor Green
+    useColor False  = inColor Red
+    usePrefix True  = "is"
+    usePrefix False = "is not"
+
+
+tryStrategies :: Context -> Assertion -> [Strategy] -> IO Result
+tryStrategies _ _ [] = return Nothing
+tryStrategies ctx x (s:ss) = do
+  result <- s ctx x
+  if isJust result 
+    then return result 
+    else tryStrategies ctx x ss 
 
 
 isIn :: Assertion -> AC -> Bool
@@ -76,21 +122,25 @@ x `isIn` (AC xs) = x `elem` xs
 -- --------------------------------------------------
 --                  AC, D |= A says f
 --
-cond' :: Context -> Assertion -> Assertion -> Maybe (Proof Assertion)
+cond' :: Context -> Assertion -> Assertion -> IO Result
 cond' ctx result query =
-    let w = who query
-        whoSays = asserts w
-        fs = conditions (says query)
-        aSaysFs = map whoSays fs
-        ctx' = ctx{theta=[]}
-    in
-      makeCond (ctx,result) 
-               (map (ctx' ||-) aSaysFs) 
-               (ctx' ||- (constraint . says $ query)) 
-               (flat . fact . says $ query)
+  let w = who query
+      whoSays = asserts w
+      fs = conditions (says query)
+      aSaysFs = map whoSays fs
+      ctx' = ctx{theta=[]}
+  in do
+    ifStatements <- mapM (ctx' ||-) aSaysFs
+    cs <- ctx' ||- (constraint . says $ query)
+    return $
+      makeCond 
+        (ctx,result) 
+        ifStatements
+        cs
+        (flat . fact . says $ query)
 
 
-cond :: Context -> Assertion -> Assertion -> Maybe (Proof Assertion)
+cond :: Context -> Assertion -> Assertion -> IO Result
 cond ctx result query =
   let query'        = simplify query
       renaming      = fromJust $ result ==? query'
@@ -109,8 +159,8 @@ cond ctx result query =
 -- AC, oo |= A says B can-say D fact    AC, D |= B says factv
 -- ---------------------------------------------------------
 --                     AC, oo |= A says fact
-canSay' :: Context -> Assertion -> Assertion -> Maybe (Proof Assertion)
-canSay' Context{d=Zero} _ _ = Nothing
+canSay' :: Context -> Assertion -> Assertion -> IO Result
+canSay' Context{d=Zero} _ _ = return Nothing
 canSay' ctx query canSayStm = 
   let w = who query
       f = fact . says $ query
@@ -120,12 +170,17 @@ canSay' ctx query canSayStm =
       b = subject cs
       ctx' = ctx{theta=[]}
   in if f == f'
-       then makeCanSay (ctx,query) 
-                       (ctx' ||- delegates w b f de) 
-                       (ctx'{d=de} ||- (b `asserts` f))
-       else Nothing
+       then do
+         del <- ctx' ||- delegates w b f de 
+         ass <- ctx'{d=de} ||- (b `asserts` f)
+         return $
+           makeCanSay 
+             (ctx,query) 
+             del
+             ass
+       else return Nothing
 
-canSay :: Context -> Assertion -> Assertion -> Maybe (Proof Assertion)
+canSay :: Context -> Assertion -> Assertion -> IO Result
 canSay ctx result canSayStm = 
   let canSayStm'    = simplify canSayStm
       pRenaming     = result ==? canSayStm'
@@ -159,27 +214,32 @@ delegates from to w level =
                              }
               }
 
-tryStated :: Context -> Assertion -> Maybe (Proof Assertion)
+tryStated :: Context -> Assertion -> IO Result
 tryStated ctx a =  
   let as = filter (isJust . (==? a)) (acs $ ac ctx)
   in case as of
-    [] -> Nothing
+    [] -> return Nothing
     (p:_) -> 
       let ts = fromJust $ p ==? a
-      in Just $ PStated (ctx{theta=ts}, p)
+      in 
+        return . Just $ PStated (ctx{theta=ts}, p)
 
-tryCond :: Context -> Assertion -> Maybe (Proof Assertion)
+tryCond :: Context -> Assertion -> IO Result
 tryCond ctx a = 
     let as = filter (isSpecific a) (acs (ac ctx))
-    in case filter isJust $ map (cond ctx a) as of
+    in do
+      candidates <- mapM (cond ctx a) as
+      return $ case filter isJust candidates of
          [] -> Nothing
          (p:_) -> p
 
 
-tryCanSay :: Context -> Assertion -> Maybe (Proof Assertion)
+tryCanSay :: Context -> Assertion -> IO Result
 tryCanSay ctx a = 
     let as = filter (isDelegation a) (acs (ac ctx))
-    in case filter isJust $ map (canSay ctx a) as of
+    in do
+      candidates <- mapM (canSay ctx a) as
+      return $ case filter isJust candidates of
          [] -> Nothing
          (p:_) -> p
 
--- tests/Tests/Evaluation.hs
+++ tests/Tests/Evaluation.hs
@@ -7,9 +7,8 @@ import Text.Parsec
 import Logic.SecPAL.Evaluable
 import Logic.SecPAL.Context
 import Tests.Testable
-import Data.Either
 import Data.Maybe
-
+import System.IO.Unsafe (unsafePerformIO)
 
 makeAssertionUnsafe :: String -> Assertion
 makeAssertionUnsafe x = case parse pAssertionUnsafe "" x of
@@ -21,34 +20,42 @@ makeAssertion x = case parse pAssertion "" x of
   (Left err) -> error . show $ err
   (Right a) -> a
 
+testEvaluationTruths :: [Test] 
 testEvaluationTruths = [inACTest1, condNoRename1]
+testEvaluationFalsehoods :: [Test] 
 testEvaluationFalsehoods = [falseInACTest1, falseCondNoRename1]
 
+testCanSay :: [Test] 
 testCanSay = [canSay01, canSayInf1]
+testCanSayF :: [Test] 
 testCanSayF = [canSayInfFalse1]
 
+testRenamingEval :: [Test] 
 testRenamingEval = [ condRename1, condRename2 , canSayRename1, testESSoSExample ]
 
+testFunctions :: [Test] 
 testFunctions = [ testHasPermission, testHasntPermission, testHasntPermission2 ]
 
 -- An assertion is true if it is in the assertion context
+inACTest1 :: Test 
 inACTest1 = 
   let 
     a = makeAssertionUnsafe "Alice says Bob is-cool."
     ctx = stdCtx{ ac=AC [a], d=Infinity }
-    prf = ctx ||- a
+    prf = unsafePerformIO $ ctx ||- a
     pPrf = maybe "" (\p -> '\n':pShow p) prf
   in
     Test{ description = pShow ctx ++" |= "++pShow a ++ pPrf
         , result = test . isJust $ prf
         }
 
+falseInACTest1 :: Test 
 falseInACTest1 = 
   let 
     a = makeAssertionUnsafe "Alice says Alice is-cool."
     b = makeAssertionUnsafe "Alice says Bob is-cool."
     ctx = stdCtx{ ac=AC [a], d=Infinity }
-    prf = ctx ||- b
+    prf = unsafePerformIO $ ctx ||- b
     pPrf = maybe "" (\p -> '\n':pShow p) prf
   in
     Test{ description = pShow ctx ++" |= "++pShow b++pPrf
@@ -56,13 +63,14 @@ falseInACTest1 =
         }
 
 -- Can we use the cond variable without renaming
+condNoRename1 :: Test 
 condNoRename1 =
   let 
     a = makeAssertionUnsafe "Alice says Bob is-cool."
     a' = makeAssertionUnsafe "Alice says Bob is-cool if Bob likes-jazz."
     b = makeAssertionUnsafe "Alice says Bob likes-jazz."
     ctx = stdCtx{ ac=AC [a', b], d=Infinity }
-    prf = ctx ||- a
+    prf = unsafePerformIO $ ctx ||- a
     pPrf = maybe "" (\p -> '\n':pShow p) prf
   in
     Test{ description = pShow ctx ++" |= "++pShow a++pPrf
@@ -70,87 +78,95 @@ condNoRename1 =
         }
         --
 
+falseCondNoRename1 :: Test 
 falseCondNoRename1 =
   let 
     a = makeAssertionUnsafe "Alice says Bob is-cool."
     a' = makeAssertionUnsafe "Alice says Bob is-cool if Bob likes-jazz."
     b = makeAssertionUnsafe "Alice says Bob likes-jazz; False."
     ctx = stdCtx{ ac=AC [a', b], d=Infinity }
-    prf = ctx ||- a
+    prf = unsafePerformIO $ ctx ||- a
     pPrf = maybe "" (\p -> '\n':pShow p) prf
   in
     Test{ description = pShow ctx ++" |= "++pShow a++pPrf
         , result = test . isNothing $ prf
         }
 
+canSay01 :: Test 
 canSay01 =
   let q  = makeAssertionUnsafe "Bob says Alice likes-jazz."
       a1 = makeAssertionUnsafe "Bob says Alice can-say 0 Alice likes-jazz."
       a2 = makeAssertionUnsafe "Alice says Alice likes-jazz."
       ctx = stdCtx{ ac=AC [a1,a2], d=Infinity }
-      prf = ctx ||- q
+      prf = unsafePerformIO $ ctx ||- q
       pPrf = maybe "" (\p -> '\n':pShow p) prf
   in Test { description = pShow ctx ++ " |= " ++ pShow q ++ pPrf
           , result = test . isJust $ prf
           }
 
+canSayInf1 :: Test 
 canSayInf1 =
   let q  = makeAssertionUnsafe "Bob says Alice likes-jazz."
       a1 = makeAssertionUnsafe "Bob says Alice can-say inf Alice likes-jazz."
       a2 = makeAssertionUnsafe "Alice says Clive can-say 0 Alice likes-jazz."
       a3 = makeAssertionUnsafe "Clive says Alice likes-jazz."
       ctx = stdCtx{ ac=AC [a1,a2,a3], d=Infinity }
-      prf = ctx ||- q
+      prf = unsafePerformIO $ ctx ||- q
       pPrf = maybe "" (\p -> '\n':pShow p) prf
   in Test { description = pShow ctx ++ " |= " ++ pShow q ++ pPrf
           , result = test . isJust $ prf
           }
 
+canSayInfFalse1 :: Test 
 canSayInfFalse1 =
   let q  = makeAssertionUnsafe "Bob says Alice likes-jazz."
       a1 = makeAssertionUnsafe "Bob says Alice can-say 0 Alice likes-jazz."
       a2 = makeAssertionUnsafe "Alice says Clive can-say 0 Alice likes-jazz."
       a3 = makeAssertionUnsafe "Clive says Alice likes-jazz."
       ctx = stdCtx{ ac=AC [a1,a2,a3], d=Infinity }
-      prf = ctx ||- q
+      prf = unsafePerformIO $ ctx ||- q
       pPrf = maybe "" (\p -> '\n':pShow p) prf
   in Test { description = pShow ctx ++ " |= " ++ pShow q ++ pPrf
           , result = test . isNothing $ prf
           }
 
 
+condRename1 :: Test 
 condRename1 = 
   let q    = makeAssertionUnsafe "Bob says Alice likes-jazz."
       a1   = makeAssertionUnsafe "Bob says everyone likes-jazz."
       ctx  = stdCtx{ ac=AC [a1], d=Infinity }
-      prf  = ctx ||- q
+      prf  = unsafePerformIO $ ctx ||- q
       pPrf = maybe "" (\p -> '\n':pShow p) prf
   in Test { description = pShow ctx ++ " |= " ++ pShow q ++ pPrf
           , result = test . isJust $ prf
           }
 
+condRename2 :: Test 
 condRename2 = 
   let q = makeAssertionUnsafe "Bob says Alice likes-jazz."
       a1 = makeAssertionUnsafe "Bob says everyone likes-jazz if everyone is-human."
       a2 = makeAssertionUnsafe "Bob says Alice is-human."
       ctx = stdCtx{ ac=AC [a1, a2], d=Infinity }
-      prf = ctx ||- q
+      prf = unsafePerformIO $ ctx ||- q
       pPrf = maybe "" (\p -> '\n':pShow p) prf
   in Test { description = pShow ctx ++ " |= " ++ pShow q ++ pPrf
           , result = test . isJust $ prf
           }
 
+canSayRename1 :: Test 
 canSayRename1 = 
   let q = makeAssertionUnsafe "Bob says Alice likes-jazz."
       a1 = makeAssertionUnsafe "Bob says anyone can-say 0 anyone likes-jazz."
       a2 = makeAssertionUnsafe "Alice says Alice likes-jazz."
       ctx = stdCtx{ ac=AC [a1, a2], d=Infinity }
-      prf = ctx ||- q
+      prf = unsafePerformIO $ ctx ||- q
       pPrf = maybe "" (\p -> '\n':pShow p) prf
   in Test { description = pShow ctx ++ " |= " ++ pShow q ++ pPrf
           , result = test . isJust $ prf
           }
 
+testESSoSExample :: Test 
 testESSoSExample =
   let q  = makeAssertion "Phone says Game is-installable."
       a3 = makeAssertion "Phone says app is-installable if app meets(NotMalware), app meets(NoInfoLeaks)."
@@ -161,38 +177,41 @@ testESSoSExample =
       a8 = makeAssertion "AVChecker says Game meets(NotMalware)."
       a9 = makeAssertion "NILInferer says Evidence shows-meets(Game,NoInfoLeaks)." -- Bit simplified
       ctx = stdCtx{ac=AC [a3, a4, a5, a6, a7, a8, a9]}
-      prf = ctx ||- q
+      prf = unsafePerformIO $ ctx ||- q
       pPrf = maybe "" (\p -> '\n':pShow p) prf
   in Test { description = pShow ctx ++ " |= " ++ pShow q ++ pPrf
           , result = test . isJust $ prf
           }
 
 
+testHasPermission :: Test 
 testHasPermission = 
   let q  = makeAssertion "User says App can-access-internet."
       a1 = makeAssertionUnsafe "anyone says app can-access-internet; permissionsCheck(app, \"INTERNET\") = True."
       ctx = stdCtx{ac=AC [a1]}
-      prf = ctx ||- q
+      prf = unsafePerformIO $ ctx ||- q
       pPrf = maybe "" (\p -> '\n':pShow p) prf
   in Test { description = pShow ctx ++ " |= " ++ pShow q ++ pPrf
           , result = test . isJust $ prf
           }
 
+testHasntPermission :: Test 
 testHasntPermission = 
   let q  = makeAssertion "User says App cannot-access-internet."
       a1 = makeAssertionUnsafe "anyone says app cannot-access-internet; permissionsCheck(app, \"INTERNET\") = False."
       ctx = stdCtx{ac=AC [a1]}
-      prf = ctx ||- q
+      prf = unsafePerformIO $ ctx ||- q
       pPrf = maybe "" (\p -> '\n':pShow p) prf
   in Test { description = pShow ctx ++ " |= " ++ pShow q ++ pPrf
           , result = test . isNothing $ prf
           }
 
+testHasntPermission2 :: Test 
 testHasntPermission2 = 
   let q  = makeAssertion "User says App cannot-dance."
       a1 = makeAssertionUnsafe "anyone says app cannot-dance; permissionsCheck(app, \"BOOGIE\") = False."
       ctx = stdCtx{ac=AC [a1]}
-      prf = ctx ||- q
+      prf = unsafePerformIO $ ctx ||- q
       pPrf = maybe "" (\p -> '\n':pShow p) prf
   in Test { description = pShow ctx ++ " |= " ++ pShow q ++ pPrf
           , result = test . isJust $ prf
